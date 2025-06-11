113230444f3dffdeb41febfa57ddbd0f35dbf4ba
junaidibiz <junaidibizid@gmail.com>
2025-06-11 21:21:20 +0700

    menambahkan Eloquent Relationships ke model user, campaign, category, donation, donatur

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,14 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * campaigns
+     *
+     * @return void
+     */
+    public function campaigns()
+    {
+        return $this->hasMany(Campaign::class);
+    }
 }
