2eb0408123711bce5d8da7ccd51c90713badca5b
junaidibiz <junaidibizid@gmail.com>
2025-06-12 01:10:40 +0700

    memperbaiki error pada slaider menu

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -46,41 +46,40 @@
                 </h3>
                 <ul class="mt-3">
                     <x-menu.item :route="route('dashboard')" label="Dashboard" segment="dashboard">
-                        <svg class="fill-current @if(Request::is('dashboard*')) text-damu-500 @else text-gray-400 dark:text-gray-500 @endif" viewBox="0 0 16 16">
+                        <svg class="fill-current" viewBox="0 0 16 16">
                             <path d="M5.936.278A7.983 7.983 0 0 1 8 0a8 8 0 1 1-8 8c0-.722.104-1.413.278-2.064a1 1 0 1 1 1.932.516A5.99 5.99 0 0 0 2 8a6 6 0 1 0 6-6c-.53 0-1.045.076-1.548.21A1 1 0 1 1 5.936.278Z" />
                             <path d="M6.068 7.482A2.003 2.003 0 0 0 8 10a2 2 0 1 0-.518-3.932L3.707 2.293a1 1 0 0 0-1.414 1.414l3.775 3.775Z" />
                         </svg>
                     </x-menu.item>
 
-                    {{-- Ganti '#' dengan route('kategori.index') jika sudah ada --}}
                     <x-menu.item route="#" label="Kategori" segment="kategori">
-                        <svg class="@if(Request::is('kategori*')) text-damu-500 @else text-gray-400 dark:text-gray-500 @endif" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
                         </svg>
                     </x-menu.item>
 
                     <x-menu.item route="#" label="Campaign" segment="campaign">
-                        <svg class="@if(Request::is('campaign*')) text-damu-500 @else text-gray-400 dark:text-gray-500 @endif" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
                         </svg>
                     </x-menu.item>
 
                     <x-menu.item route="#" label="Donation" segment="donation">
-                        <svg class="@if(Request::is('donation*')) text-damu-500 @else text-gray-400 dark:text-gray-500 @endif" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
                         </svg>
                     </x-menu.item>
 
                     <x-menu.item route="#" label="Donatur" segment="donatur">
-                        <svg class="@if(Request::is('donatur*')) text-damu-500 @else text-gray-400 dark:text-gray-500 @endif" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
                         </svg>
                     </x-menu.item>
 
                     <x-menu.item route="#" label="Slider" segment="slider">
-                        <svg class="@if(Request::is('slider*')) text-damu-500 @else text-gray-400 dark:text-gray-500 @endif" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                        <svg fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6" />
                         </svg>
                     </x-menu.item>                                                                
--- resources/views/components/menu/item.blade.php
+++ resources/views/components/menu/item.blade.php
@@ -3,9 +3,17 @@
 <li class="pl-4 pr-3 py-2 rounded-lg mb-0.5 last:mb-0 @if(Request::is($segment.'*')) bg-damu-100 dark:bg-damu-500/20 @endif">
     <a class="block text-gray-800 dark:text-gray-100 truncate transition hover:text-gray-900 dark:hover:text-white" href="{{ $route }}">
         <div class="flex items-center">
-            <div class="shrink-0 h-4 w-4">
+
+            {{-- Bagian ini yang kita perbaiki --}}
+            <div @class([
+                'shrink-0 h-4 w-4',
+                'text-damu-500' => Request::is($segment.'*'),
+                'text-gray-400 dark:text-gray-500' => !Request::is($segment.'*'),
+            ])>
                 {{ $slot }}
             </div>
+            {{-- Akhir perbaikan --}}
+
             <span class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">{{ $label }}</span>
         </div>
     </a>
