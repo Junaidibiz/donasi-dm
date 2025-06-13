b0cdc90e2b57ffc2f97a62ff01b8ab7366b979f8
junaidibiz <junaidibizid@gmail.com>
2025-06-13 20:01:40 +0700

    melalukan pembaruan code di applayout untuk mengaktifkan title halaman web

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -8,6 +8,24 @@ use Illuminate\View\View;
 class AppLayout extends Component
 {
     /**
+     * Judul halaman.
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * Buat instance komponen baru.
+     *
+     * @param  string  $title
+     * @return void
+     */
+    public function __construct($title = null)
+    {
+        $this->title = $title ?? config('app.name', 'Dashboard');
+    }
+
+    /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
