dbaa39d07137bb002c8fc56802921afc64a19fdb
junaidibiz <junaidibizid@gmail.com>
2025-06-14 18:59:34 +0700

    Mulai membuat API dengan API bawaan jezstream yaitu Laravel Sanctum

--- app/Models/Donatur.php
+++ app/Models/Donatur.php
@@ -2,34 +2,45 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Donatur extends Model
+class Donatur extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable;
+
     /**
-     * fillable
-     *
-     * @var array
+     * The attributes that are mass assignable.
+     * @var array<int, string>
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar'
+        'name',
+        'email',
+        'password',
+        'avatar',
+        'is_verified'
     ];
-    
+
     /**
-     * hidden
-     *
-     * @var array
+     * The attributes that should be hidden for serialization.
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
+    
     /**
-     * donations
-     *
-     * @return void
+     * The attributes that should be cast.
+     * @var array<string, string>
      */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
     public function donations()
     {
         return $this->hasMany(Donation::class);
